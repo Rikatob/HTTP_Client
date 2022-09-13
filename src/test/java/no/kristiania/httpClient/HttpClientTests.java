dfff0bebffe69bafdd8b023df4c67f98b8ae6892
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-09-13 13:46:40 +0200

    🔴 shoul get failure response code (code 403)

--- src/test/java/no/kristiania/httpClient/HttpClientTests.java
+++ src/test/java/no/kristiania/httpClient/HttpClientTests.java
@@ -12,4 +12,11 @@ public class HttpClientTests {
         assertEquals(200,client.getStatusCode());
 
     }
+
+    @Test
+    void shouldGetFailureResponseCode(){
+        HttpClient client = new HttpClient("httpbin.org",80,"/status/403");
+        assertEquals(403,client.getStatusCode());
+
+    }
 }
